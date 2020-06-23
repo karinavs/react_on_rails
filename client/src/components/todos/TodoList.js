206ae47fa22870bb248d9441015fbe99f895298f
Karina Stevens <keva86@hotmail.com>
2020-06-22 21:00:31 -0600

    final

---
+++ client/src/components/todos/TodoList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Todo from './Todo';
+
+const TodoList = ({ todos, updateTodo, deleteTodo }) => (
+  <>
+    { todos.map( t => 
+      <Todo
+        key={t.id}
+        {...t}
+        updateTodo={updateTodo}
+        deleteTodo={deleteTodo}
+      />
+    )
+    }
+  </>
+)
+export default TodoList;
